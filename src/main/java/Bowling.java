626300242623bbb0e4d2ff58221f47c83424544e
SBAYTTI Jihane <sbaytti.jihane@gmail.com>
2018-05-16 08:59:44 +0200

    Bowling game V1

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -1,6 +1,96 @@
+/**
+ * Created by jihane on 01/05/2018.
+ *  Bowling de 10 rolls de 2 rolls chacune sauf la dernière qui peut admettre 3 rolls.
+ *  Si la dernière frame est un Spare, elle admettra 1 lancé de plus.
+ *  Si la dernière frame est un Strike, elle admettra 2 lancés de plus.
+ */
 
 public class Bowling {
 
+    private int[] frameScores;
+    private int maximumFrames = 10;
+    private int[][] rolls; //stocke le nombre des pins renversés
+    private int counterRolls = 0; //numéro du roll dans la partie (0 à 19 ou 21 si bonus)
+    private int counterFrame = 0; //numéro du frame (jusqu'à 9)
+    private int score = 0;
+    private int[] bonusRolls;//size 1 if last frame is a Spare, size 2 if last frame is a Strike
 
 
+    public Bowling() {
+        this.rolls = new int[maximumFrames][2];
+        this.bonusRolls = new int[2];
+        this.frameScores = new int[maximumFrames];
+    }
+
+    //stocke les valeurs des pins renversés pendant chaque roll
+    public void roll(int pinsShooted) {
+        if (counterRolls >19) {
+            bonusRolls[(counterRolls) %2] = pinsShooted;
+        }
+        else {
+            rolls[counterFrame][counterRolls % 2] = pinsShooted;
+            if (counterRolls % 2 == 1 && counterFrame<9) {
+                counterFrame++;
+                System.out.print(counterFrame);
+            }
+        }
+        counterRolls++;
+    }
+
+    //Si la somme des éléments d’un tuple est 10 :
+    // on ajoute à la valeur de ce tuple la valeur du premier élément du tuple suivant.
+    //Si le premier élément d’un tuple vaut 10 : on ajoute à la valeur du tuple les deux prochaines valeurs
+    public int[] frameScores() {
+        for (int i = 0; i < frameScores.length ; i++) { //on s'arrête à l'avant-dernier frame
+            if ( isaSpare(rolls[i])){
+                if (counterFrame!=9) {
+                    frameScores[i] = 10 + rolls[i+1][0];
+                }else { //last frame
+                    frameScores[i] = 10 + bonusRolls[0] + bonusRolls[1];
+                }
+            }
+            else {
+                frameScores[i] = rolls[i][0] + rolls[i][1];
+            }
+            score += frameScores[i];
+        }
+
+        return frameScores;
+    }
+
+    private boolean isaSpare(int[] frame) {
+        return frame[0] + frame[1] == 10;
+    }
+
+    public int score(){
+        frameScores();
+        return score;
+    }
+
+
+
+    /*
+        public int[] frameScores() {
+        int size = frameScores.length - 1;
+        for (int i = 0; i < size; i++) {
+            if ( isaSpare(rolls[i]) ){
+                frameScores[i] = 10 + rolls[i+1][0];
+            }
+            else {
+                frameScores[i] = rolls[i][0] + rolls[i][1];
+            }
+            score += frameScores[i];
+        }
+        //dernier frame
+        if (isaSpare(rolls[size])){
+            frameScores[size] = 10 + bonusRolls[0];
+        }else {
+            frameScores[size] = rolls[size][0] + rolls[size][1];
+        }
+        score +=frameScores[size];
+
+
+        return frameScores;
+    }
+     */
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,21 +1,84 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.Before;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class BowlingTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+public class BowlingTest {
 
-    @AfterEach
-    void tearDown() {
+    private Bowling bowling;
+
+    @Before
+    public void setUp() {
+        bowling = new Bowling();
     }
 
     @Test
-    void test1(){
-        assertEquals(1, 1);
+    public void canScoreGameOfZeros(){
+        roll(20, 0);
+        assertEquals(0, bowling.score());
     }
+
+    @Test
+    public void canScoreGameOfOnes(){
+        roll(20, 1);
+        assertEquals(10, bowling.score());
+    }
+
+    @Test
+    public void canScoreSpareFollowedByThree() {
+        bowling.roll(5);
+        bowling.roll(5);
+        bowling.roll(3);
+        assertEquals(bowling.score(), 16);
+    }
+
+    @Test
+    public void canScoreGameOfFivesAndOneAtTheEnd() {
+        roll(19, 5);
+        bowling.roll(1);
+        assertEquals(141, bowling.score());
+    }
+
+    @Test
+    public void canRollGameOfSpares() {
+        roll(21, 5);
+    }
+
+    @Test
+    public void canScoreGameOfSpares() {
+        roll(21, 5);
+        assertEquals(bowling.score(), 150);
+    }
+
+
+    @Test
+    public void canScoreStrikeFollowedByThreeAndFour() {
+        bowling.roll(10);
+        bowling.roll(3);
+        bowling.roll(4);
+        assertEquals(bowling.score(), 24);
+    }
+
+    @Test
+    public void canScoreGameOfTensAndOneAtTheEnd() {
+        roll(19, 5);
+        bowling.roll(1);
+        assertEquals(141, bowling.score());
+    }
+
+    @Test
+    public void canScoreGameOfStrikes() {
+        roll(22, 10);
+        assertEquals(bowling.score(), 300);
+    }
+
+    private void roll(int times, int pinsShooted) {
+        for(int i = 0; i< times; i++) {
+            bowling.roll(pinsShooted);
+        }
+    }
+
+
+
 }
